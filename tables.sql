87e3708afce0076be23ae8e02c7f0395d26f88b3
rashmireddy <irashmi.reddy@gmail.com>
2014-12-06 00:30:12 -0800

    Create tables.sql
    SQL file which creates tables in MySql

---
+++ tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE nasdaq_daily_prices ( 
+exchange VARCHAR(35) ,
+stock_symbol VARCHAR(35) , 
+date VARCHAR(35) , 
+stock_price_open DOUBLE , 
+stock_price_high DOUBLE , 
+stock_price_low DOUBLE, 
+stock_price_close DOUBLE, 
+stock_volume BIGINT, 
+stock_price_adj_close DOUBLE
+) engine=InnoDB;
